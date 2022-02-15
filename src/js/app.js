3c1c1fbbbc7f76506026b6f3d8316c67e78d9ae8
Robert Graupner <robert.graupnerr@gmail.com>
2022-02-15 13:58:31 +0100

    add html template

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,23 @@
-import { settings } from "./settings.js";
+import { settings } from './settings.js';
 
 const app = {
   initData: function () {
-    const thisApp = this;
-		const url = settings.db.url + "/" + settings.db.products;
-		this.data = {};
-		fetch(url)
-			.then((rawResponse) => {
-				return rawResponse.json();
-			})
-			.then((parsedResponse) => {
-				this.data.products = parsedResponse;
-			});
-	},
+    //const thisApp = this;
+    const url = settings.db.url + '/' + settings.db.products;
+    this.data = {};
+    fetch(url)
+      .then((rawResponse) => {
+        return rawResponse.json();
+      })
+      .then((parsedResponse) => {
+        this.data.products = parsedResponse;
+      });
+  },
 
-	init: function () {
-		const thisApp = this;
-		thisApp.initData();
-	},
+  init: function () {
+    const thisApp = this;
+    thisApp.initData();
+  },
 };
 
 app.init();
